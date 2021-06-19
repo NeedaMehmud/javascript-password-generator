21ed266b5d74d7e232ea7ac45c43cadeee424dce
Needa Mehmud <needa.mehmud@gmail.com>
2021-06-18 22:51:08 -0400

    resolved bug with printing password

--- main/js/script.js
+++ main/js/script.js
@@ -2,64 +2,64 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function createPassword() {
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
 
   passwordText.value = password;
+}
+var password = [];
 
-  this.addEventListener("click", function () {
-    passwordText = generatePassword();
-  });
-} 
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Function to generate password
-function generatePassword(){
+function generatePassword() {
+  var requiredCharacterString = "";
   // password variation
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numeric = "0123456789";
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var nonAlphanumeric = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-  var numericalPassword;
-  var specialCharacters;
-  var lowercasePassword;
-  var uppercasePassword;
-  var characterLength;
-
   // users are asked to select which type of password they would like to generate
-    var startGeneratingPassword = parseInt(prompt(
+  var characterLength = parseInt(prompt(
     "Welcome to password generator! \n How many characters would you like to have in your password?"
-    ));
+  ));
 
-    if (!startGeneratingPassword) {
-      alert("The field can not be empty");
+  if (!characterLength) {
+    alert("The field can not be empty");
+  } else if (characterLength < 8 || characterLength > 128) {
+    characterLength.parseInt(prompt("Please submit a password between 8 and 128 characters."));
+  }
 
-  if (characterLength < 8 || characterLegth > 128) {
-    window.alert("Please submit a password between 8 and 128 characters.");
-  } 
-  else {
-    numericalPassword = confirm("Do you want your password to contain numbers?");
-    specialCharacters = confirm("Do you want your password to contain special characters?");
-    lowercasePassword = confirm("Do you want your password to contain uppercase letters?");
-    uppercasePassword = confirm("Do you want your password to contain lowercase letters?");
-  };
+  var numericalPassword = confirm("Do you want your password to contain numbers?");
 
-  return generatePassword;
+  if (numericalPassword) {
+    requiredCharacterString = requiredCharacterString + numeric;
+    console.log(requiredCharacterString);
+  }
+
+  var specialCharacters = confirm("Do you want your password to contain special characters?");
+  if (specialCharacters) {
+    requiredCharacterString = requiredCharacterString + nonAlphanumeric;
+  }
+
+  var lowercasePassword = confirm("Do you want your password to contain uppercase letters?");
+  if (lowercasePassword) {
+    requiredCharacterString = requiredCharacterString + lowercase;
+  }
+
+  var uppercasePassword = confirm("Do you want your password to contain lowercase letters?");
+  if (uppercasePassword) {
+    requiredCharacterString = requiredCharacterString + uppercase;
+  }
+
+  for (var i = 0; i < characterLength; i++) {
+    var passwordChar = requiredCharacterString[Math.floor(Math.random() * requiredCharacterString.length)];
+    console.log(passwordChar);
+  }
+
+  return password;
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", createPassword);
